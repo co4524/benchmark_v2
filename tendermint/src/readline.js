02d38bde167abffd81f04f96a2939f1c19f60296
Jay <jay_zheng@htc.com>
2019-08-27 16:48:39 +0800

    use line-by-line module in readline.js in tendermint/src/

--- tendermint/src/readline.js
+++ tendermint/src/readline.js
@@ -1,12 +1,34 @@
-const fs = require('fs');
-const readline = require('readline');
+// const fs = require('fs');
+// const readline = require('readline');
+//
+//
+// var inputStream = fs.createReadStream('RawTx');
+// // 將讀取資料流導入 Readline 進行處理
+// var lineReader = readline.createInterface({ input: inputStream });
+// lineReader.on('line', function(line) {
+//
+//     // 取得一行行結果
+//     console.log('NEW LINE', line);
+// });
 
+// reference: https://www.npmjs.com/package/line-by-line
 
-var inputStream = fs.createReadStream('RawTx');
-// 將讀取資料流導入 Readline 進行處理 
-var lineReader = readline.createInterface({ input: inputStream });
-lineReader.on('line', function(line) {
+const lr = new (require('line-by-line'))('RawTx')
 
-    // 取得一行行結果
-    console.log('NEW LINE', line);
-});
+var i = 0
+
+lr.on('error', err => console.log(err))
+
+lr.on('line', it => {
+  console.log('new line', it)
+
+  if (++i > 5) {
+    lr.pause()
+
+    setTimeout(() => {
+      lr.resume()
+    }, 1000)
+  }
+})
+
+lr.on('end', () => console.log('finished reading'))
