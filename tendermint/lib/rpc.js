174771b67bfa1cf34faca93165abd80bfc9f10ce
Jay <jay_zheng@htc.com>
2019-08-28 12:36:51 +0800

    refine lib/rpc.js in tendermint

--- tendermint/lib/rpc.js
+++ tendermint/lib/rpc.js
@@ -1,25 +1,40 @@
-const request = require('request-promise');
+const request = require('request-promise')
 
+const _tendermintInfo = async (baseURL) => {
+  try {
 
-async function tendermintInfo( baseURL ) {
-    try {
-        let result;
-        let error;
-        let options = {
-            url: baseURL + '/status',
-        };
+    return JSON.parse(await request.get(`${baseURL}/status`))
 
-        await request.get(options, (err, res, body) => {
-            if (err) error = err;
-            result = res;
-        });
-        if (error) throw error;
-
-        return JSON.parse(result.body);
-    } catch (err) {
-        throw err;
-    }
+  } catch (err) {
+    throw err
+  }
 }
+
+const sendTx = (baseURL, tx) => {
+  request({ method: 'POST', url: `${baseURL}/broadcast_tx_commit?tx=${tx}` })
+    .catch(err => { throw err })
+}
+
+
+// async function tendermintInfo( baseURL ) {
+//     try {
+//         let result;
+//         let error;
+//         let options = {
+//             url: baseURL + '/status',
+//         };
+//
+//         await request.get(options, (err, res, body) => {
+//             if (err) error = err;
+//             result = res;
+//         });
+//         if (error) throw error;
+//
+//         return JSON.parse(result.body);
+//     } catch (err) {
+//         throw err;
+//     }
+// }
 
 async function sendTx( baseURL , tx) {
 
@@ -31,10 +46,10 @@ async function sendTx( baseURL , tx) {
         request.post(options, (err) => {
             if (err) error = err;
         });
-         
+
 }
 
 module.exports = {
     sendTx: sendTx ,
-    tendermintInfo: tendermintInfo
+    tendermintInfo: _tendermintInfo
 }
