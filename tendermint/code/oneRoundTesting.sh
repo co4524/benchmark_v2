e072cc406bbe7741ba96a119772938376509e19b
co4524 <monster3697@gmail.com>
2019-09-03 18:07:11 +0000

    shell script update

--- tendermint/code/oneRoundTesting.sh
+++ tendermint/code/oneRoundTesting.sh
@@ -1,6 +1,7 @@
 gcloud compute --project "caideyi" ssh --zone "asia-east1-b" "tendermint0" -- './Benchmarking/t-tendermint/nodeScript/dataReset.sh'
-node workload.js $1 $2 $3 $4 $5
-let end_index=$1*$2+$5
+node ../../bin/workload.js "tendermint" $1 $2 $3 $4 $5
+let end_index=$1*$2*$3+$5
 node preprocess.js $5 $end_index
 gcloud compute --project "caideyi" ssh --zone "asia-east1-b" "tendermint0" -- "./Benchmarking/t-tendermint/nodeScript/scp.sh"
-node calculate.js $1 $2 $3
+node ../../bin/calculate.js "tendermint" $1 $2 $3
+echo $end_index
