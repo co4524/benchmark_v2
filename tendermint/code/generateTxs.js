1257f6bc7730fdffcae237f1ff0e535d826ba3df
Jay <jay_zheng@htc.com>
2019-09-03 11:50:50 +0800

    refine generateTxs.js

--- tendermint/code/generateTxs.js
+++ tendermint/code/generateTxs.js
@@ -1,72 +1,69 @@
-const ethTx = require('ethereumjs-tx');
-const fs = require('fs');
-const API = require('../lib/rpc.js')
-const URL = require("../data/baseURL.json")
-const PATH_CONFIGURE =  require('../configure.json')
-const PATH_HOME = PATH_CONFIGURE.home_path
-const PATH_PRIVKEY = `${PATH_HOME}/benchmark_v2/tendermint/data/testAccount-privKey`
-const PATH_RAW_TX = `${PATH_HOME}/benchmark_v2/tendermint/res/RawTx`
-const PATH_HASH_TX = `${PATH_HOME}/benchmark_v2/tendermint/res/HashTx`
-const DES_ADDRESS = '0x6666666666666666666666666666666666666666'   //The address you want to send money
+const sendTx = require('../lib/rpc.js').sendTx
+const ethTx = require('ethereumjs-tx')
+const fs = require('fs')
 
-const app = {
-    raw_tx_size : 1000000 ,
-    batch_size : 10000 
+const config = require('../../configure.json')
+const DES_ADDRESS = '0x6666666666666666666666666666666666666666' // The address you want to send money
+
+// const URL = require("../data/baseURL.json")
+// const PATH_CONFIGURE =  require('../configure.json')
+// const PATH_HOME = PATH_CONFIGURE.home_path
+// const PATH_PRIVKEY = `${PATH_HOME}/benchmark_v2/tendermint/data/testAccount-privKey`
+// const PATH_RAW_TX = `${PATH_HOME}/benchmark_v2/tendermint/res/RawTx`
+// const PATH_HASH_TX = `${PATH_HOME}/benchmark_v2/tendermint/res/HashTx`
+
+const size = {
+  batch: 10000,
+  raw_txs: 1000000
 }
 
-async function generateRawTxAndHash(){
+// reset output data
 
-  let raw_txs = []
-  let hash_list = []
+fs.existsSync(config.tendermint.path.raw_tx) && fs.unlinkSync(config.tendermint.path.raw_tx)
+fs.existsSync(config.tendermint.path.raw_tx_hash) && fs.unlinkSync(config.tendermint.path.raw_tx_hash)
 
-  while (app.raw_tx_size--) {   // generate ${raw_tx_size} transactions
+// private keys
 
+const priv_key = fs.readFileSync(config.tendermint.path.private_key, 'utf-8').split('\n').slice(0, -1).map(raw => {
+  return Buffer.from(raw.match(/(0x[a-f0-9]+)/g))
+})
+
+;(async function () {
+
+  let raw_txs = [], raw_tx_hashes = {}
+
+  // generate transactions
+
+  while (size.raw_txs--) {
     let transaction = new ethTx({
-      nonce: app.raw_tx_size,
+      nonce: size.raw_txs,
       to: DES_ADDRESS,
       gasLimit: '0x30000',
       value: '0x01'
     })
 
-    // sign transaction & push it in array
-    transaction.sign(priv_key[app.raw_tx_size % priv_key.length])
+    // sign transactions
+
+    transaction.sign(priv_key[size.raw_txs % priv_key.length])
     raw_txs.push('0x' + transaction.serialize().toString('hex'))
 
-    if(raw_txs.length==app.batch_size){     // generate ${batch_size} raw_transactions & transaction hash
-      console.log(`Generate ${app.batch_size} transations & hash , Remaining amount : ${app.raw_tx_size}`)
-      for (let i = 0 ; i < app.batch_size ; i ++){
-        hash_list[i] = API.sendTx(URL[i%URL.length], raw_txs[i])
+    // get transaction hash and output
+
+    if (raw_txs.length === size.batch) {
+      console.log(`Generate ${size.batch} transations & hash , Remaining amount : ${size.raw_txs}`)
+
+      let receipts = await Promise.all(raw_txs.map((raw_tx, i) => sendTx(config.tendermint.urls[i % config.tendermint.urls.length], raw_tx)))
+
+      for (let i in raw_txs) {
+        raw_tx_hashes[raw_txs[i]] = JSON.parse(receipts).result.hash
       }
 
-      Promise.all(hash_list)
-      for(let i=0 ; i < hash_list.length ; i++){
-          hash_list[i] = JSON.parse(await hash_list[i]).result.hash
-      }
+      fs.appendFileSync(config.tendermint.path.raw_tx, `${raw_txs.join('\n')}\n`)
 
-      fs.appendFileSync(PATH_RAW_TX, raw_txs.join('\n'))
-      fs.appendFileSync(PATH_RAW_TX, "\n")
-      fs.appendFileSync(PATH_HASH_TX, hash_list.join('\n'))
-      fs.appendFileSync(PATH_HASH_TX, "\n")
       raw_txs = []
-      hash_list = []
     }
-
   }
 
-}
+  fs.writeFileSync(config.tendermint.path.raw_tx_hash, JSON.stringify(raw_tx_hashes))
 
-// reset data
-if (fs.existsSync(PATH_RAW_TX))
-  fs.unlinkSync(PATH_RAW_TX)
-
-// reset data
-if (fs.existsSync(PATH_HASH_TX))
-  fs.unlinkSync(PATH_HASH_TX)
-
-//  Get testing account private key
-var priv_key = fs.readFileSync(PATH_PRIVKEY, 'utf-8').split('\n').slice(0, -1).map(raw => {
-  return Buffer.from(raw.match(/(0x[a-f0-9]+)/g))
-})
-
-// generate raw_txs & hash batch by batch
-generateRawTxAndHash()
+})()
