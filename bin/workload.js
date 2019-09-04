6b04c5cfb89ffc3a67fe6fd46b617efbd2556500
Jay <jay_zheng@htc.com>
2019-09-04 14:28:08 +0800

    refine workload.js

--- bin/workload.js
+++ bin/workload.js
@@ -1,55 +1,35 @@
-const sendTx = require(`../${process.argv[2]}/lib/rpc.js`).sendTx
-const fs = require('fs')
-const config =  require('../configure.json')
+const CONSENSUS = process.argv[2]
+const [INPUT_RATE, DURATION_TIME, REPEAT, SLEEP_TIME, SLICE] = process.argv.slice(3, 8).map(it => parseInt(it))
 
-const INPUT_RATE = parseInt( process.argv[3] ,10)
-const DURATION_TIME = parseInt( process.argv[4] ,10)
-const REPEAT = parseInt( process.argv[5] ,10)
-const SLEEP_TIME = parseInt( process.argv[6] ,10)
-const SlICE = parseInt( process.argv[7] ,10)  // for testing
+const sendTx = require(`../${CONSENSUS}/lib/rpc.js`).sendTx
+
+const config = require('../configure.json')
+const raw_tx_hash = require(config[CONSENSUS].path.raw_tx_hash)
+const txs = Object.keys(raw_tx_hash).slice(SLICE) // SLICE for testing
 
 const app = {
   max_txs_in_period: INPUT_RATE * DURATION_TIME,
-  max_txs_in_round: INPUT_RATE * DURATION_TIME * REPEAT,
-  sent_log: '',
-  sent_tx: 0
-}
-console.log(app)
-
-// reset txrequest_time file
-if (fs.existsSync(config[process.argv[2]].path.tx_request_time))
-  fs.unlinkSync(config[process.argv[2]].path.tx_request_time)
- 
-const txs = fs.readFileSync(config[process.argv[2]].path.raw_tx, 'utf-8').split('\n').slice(SlICE)  //SLICE for testing
-
-const onePeriodTest = async () => {
-	app.start = 0
-	return new Promise((resolve , reject) => {
-		let interval = setInterval(() => {
-			console.log(`Send transaction ${Date.now()}`)
-			for (let i = 0; i < INPUT_RATE; i++) {
-				sendTx(config[process.argv[2]].urls[i%config[process.argv[2]].urls.length], txs[app.sent_tx++])
-				app.sent_log += `${txs[app.start++]}, ${Date.now()}\n`
-			}
-
-			if (app.start == app.max_txs_in_period){
-				clearInterval(interval)
-				fs.appendFileSync(config[process.argv[2]].path.tx_request_time, app.sent_log)
-				app.sent_log = ''
-				resolve()
-			}
-		}, 1000)
-	})
+  max_txs_in_round:  INPUT_RATE * DURATION_TIME * REPEAT,
+  sent_txs: 0
 }
 
-;(async function () {
-	await onePeriodTest()
-	console.log(app.sent_tx , app.max_txs_in_round)
-	let interval = setInterval( async () => {
-		await onePeriodTest()
-		console.log(app.sent_tx , app.max_txs_in_round)
-		if(app.sent_tx==app.max_txs_in_round){
-			clearInterval(interval)
-		}
-	}, (DURATION_TIME + SLEEP_TIME) * 1000)
-})()
+const onePeriodTest = () => {
+  let period = setInterval(() => {
+    for (let i = 0; i < INPUT_RATE; i++) {
+      sendTx(config[CONSENSUS].urls[i % config[CONSENSUS].urls.length], txs[app.sent_txs++])
+    }
+
+    if (0 === app.sent_txs % app.max_txs_in_period) {
+      clearInterval(period)
+    }
+  }, 1000)
+}
+
+let round = setInterval(() => {
+  if (app.sent_txs === app.max_txs_in_round) {
+    return clearInterval(round)
+  }
+
+  onePeriodTest()
+
+}, (DURATION_TIME + SLEEP_TIME) * 1000)
