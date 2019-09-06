5a17dace100dd7283ac5457a9dd93651551d696f
Jay <jay_zheng@htc.com>
2019-09-06 10:35:01 +0800

    refine param in rpc.js in tendermint

--- tendermint/lib/rpc.js
+++ tendermint/lib/rpc.js
@@ -4,7 +4,7 @@ module.exports = {
 
     getTransactionReceipt: (baseURL, tx_hash) => request(`${baseURL}/tx?hash=0x${tx_hash}`),
 
-    sendTx: (baseURL, tx) => request({ method: 'POST', url: `${baseURL}/broadcast_tx_async?tx=${tx}` }),
+    sendTx: (baseURL, tx) => request({ method: 'POST', url: `${baseURL}/broadcast_tx_async?tx="${tx}"` }),
 
     tendermintInfo: (baseURL) => request(`${baseURL}/status`)
 
