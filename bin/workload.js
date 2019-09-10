6aaec63698ac0c4a9694c871c1a542fda25fc928
co4524 <monster3697@gmail.com>
2019-09-10 10:02:08 +0000

    fix workload.js bug & modify post_process

--- bin/workload.js
+++ bin/workload.js
@@ -1,6 +1,6 @@
 const CONSENSUS = process.argv[2]
 const [INPUT_RATE, DURATION_TIME, REPEAT, SLEEP_TIME, SLICE] = process.argv.slice(3, 8).map(it => parseInt(it))
-
+const fs = require('fs')
 const sendTx = require(`../${CONSENSUS}/lib/rpc.js`).sendTx
 
 const config = require('../configure.json')
@@ -10,16 +10,19 @@ const txs = Object.keys(raw_tx_hash).slice(SLICE) // SLICE for testing
 const app = {
   max_txs_in_period: INPUT_RATE * DURATION_TIME,
   max_txs_in_round:  INPUT_RATE * DURATION_TIME * REPEAT,
-  sent_txs: 0
+  sent_txs: 0,
+  log: ''
 }
 
 const onePeriodTest = () => {
   let period = setInterval(() => {
+    console.log( "sendtx" , app.sent_txs ) 
     for (let i = 0; i < INPUT_RATE; i++) {
-      sendTx(config[CONSENSUS].urls[i % config[CONSENSUS].urls.length], txs[app.sent_txs++])
+      sendTx(config[CONSENSUS].urls[i % config[CONSENSUS].urls.length], txs[app.sent_txs++]).then((it) => app.log+= `${JSON.parse(it).result.hash},${JSON.parse(it).result.time}\n`)
     }
 
     if (0 === app.sent_txs % app.max_txs_in_period) {
+      console.log( "sendtx" , app.sent_txs ) 
       clearInterval(period)
     }
   }, 1000)
@@ -27,9 +30,10 @@ const onePeriodTest = () => {
 
 let round = setInterval(() => {
   if (app.sent_txs === app.max_txs_in_round) {
+    fs.writeFileSync(config.tendermint.path.transaction_log, app.log)
     return clearInterval(round)
   }
-
+  console.log(app.sent_txs, app.max_txs_in_round)
   onePeriodTest()
 
 }, (DURATION_TIME + SLEEP_TIME) * 1000)
--- tendermint/code/post_process.js
+++ tendermint/code/post_process.js
@@ -2,61 +2,27 @@ const getTransactionReceipt = require(`../lib/rpc.js`).getTransactionReceipt
 const fs = require('fs')
 const config =  require('../../configure.json')
 const transaction_log = fs.readFileSync(config.tendermint.path.transaction_log, 'utf-8').split('\n')
+const block_commit_time = fs.readFileSync(config.tendermint.path.block_commit_time, 'utf-8').split('\n').slice(0,-1)   // 28,50,1568105264997   => block_number,transaction_number,block_commit_time
 
 const app = {
-    hash : [],
-    hash_info : {},   // "tendermintHash": {"time" : "timeStamp" , "blockheight" : "blockHeight"}
-    blockHeight_time : {},
-    block : []
+    total_txs : 0,
+    obj : {} ,
 }
 
-fs.existsSync(config.tendermint.path.tx_request_time) && fs.unlinkSync(config.tendermint.path.tx_request_time)
-fs.existsSync(config.tendermint.path.block_tx_num) && fs.unlinkSync(config.tendermint.path.block_tx_num)
-
-let log = ''
-transaction_log.map((it,index) => {
-    if(index%11==0 && index!=0) {
-        app.hash_info[JSON.parse(log).result.hash]= {"time": JSON.parse(log).result.time , "blockHeight": ""}
-        log = ''
+block_commit_time.map((it) => {
+    app.obj[`block${it.split(',')[0]}`]={
+        "timeStamp" : it.split(',')[2],
+        "transactions" : []
     }
-    log+=`${it}\n`
+    app.total_txs+=parseInt(it.split(',')[1])
 })
-app.hash = Object.keys(app.hash_info)
 
 ;(async function () {
 
-    for (let i = 0 ; i < app.hash.length ; i++){
-        app.hash_info[app.hash[i]].blockHeight = JSON.parse( await getTransactionReceipt(config.tendermint.urls[i%config.tendermint.urls.length], app.hash[i]) ).result.height  
+    for (let i = 0 ; i < app.total_txs ; i++){
+        let tx_block_height = JSON.parse( await getTransactionReceipt(config.tendermint.urls[i%config.tendermint.urls.length], transaction_log[i].split(',')[0]) ).result.height
+        app.obj[`block${tx_block_height}`].transactions.push(transaction_log[i])
     } 
-
-    app.blockHeight_time[ app.hash_info[app.hash[0]].blockHeight ]= [app.hash_info[app.hash[0]].time]
-    app.block.push(app.hash_info[app.hash[0]].blockHeight)
-    for (let i = 1 ; i < app.hash.length ; i++){
-        let blockHeight = app.hash_info[app.hash[i]].blockHeight
-        let newObj = true
-        for(let j = 0 ; j < app.block.length ; j++){
-            if(blockHeight===app.block[j]) {
-                newObj = false
-                app.blockHeight_time[ blockHeight ].push(app.hash_info[app.hash[i]].time)
-                break
-            }
-        }
-        if(newObj) {
-            app.blockHeight_time[ blockHeight ] = [app.hash_info[app.hash[i]].time]
-            app.block.push(blockHeight)
-        }
-    }
-    
-    app.block = Object.keys(app.blockHeight_time) ;
-    for (let i = 0 ; i < app.block.length ; i++){
-        let time_seqence = app.blockHeight_time[app.block[i]]
-        let time = ''
-        fs.appendFileSync(config.tendermint.path.block_tx_num, `${app.blockHeight_time[app.block[i]].length}\n`)
-        for (let j = 0 ; j < app.blockHeight_time[app.block[i]].length ; j++){
-            time += `${time_seqence[j]}\n`
-        }
-        fs.appendFileSync(config.tendermint.path.tx_request_time, time)
-    }
-
-
+    fs.writeFileSync(config.tendermint.path.post_process, JSON.stringify(app.obj, null, '\t'))
 })()
+
