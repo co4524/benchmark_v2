d72626dc6588b81bb53df13c2cc59175c99a5aec
co4524 <monster3697@gmail.com>
2019-09-11 08:10:29 +0000

    fix bug

--- tendermint/code/oneRoundTesting.sh
+++ tendermint/code/oneRoundTesting.sh
@@ -1,7 +1,8 @@
 gcloud compute --project "caideyi" ssh --zone "asia-east1-b" "tendermint0" -- 'rm /home/caideyi/Benchmarking/t-tendermint/data/block_commit_time'
 node ../../bin/workload.js "tendermint" $1 $2 $3 $4 $5
+#[INPUT_RATE,DURATION_TIME,REPEAT,SLEEP_TIME,SLICE]
 let end_index=$1*$2*$3+$5
 gcloud compute --project "caideyi" ssh --zone "asia-east1-b" "tendermint0" -- "./Benchmarking/t-tendermint/nodeScript/scp.sh"
-node post_process.js $1 $2
+node post_process.js $1 $2 $3 $5
 node ../../bin/calculate.js "tendermint" $1 $2 $3
 echo $end_index
--- tendermint/code/post_process.js
+++ tendermint/code/post_process.js
@@ -2,11 +2,12 @@ const fs = require('fs')
 const getTransactionReceipt = require(`../lib/rpc.js`).getTransactionReceipt
 
 const config = require('../../configure.json')
-const [INPUT_RATE, DURATION_TIME, REPEAT] = process.argv.slice(2, 5).map(it => parseInt(it))
+//const [INPUT_RATE, DURATION_TIME, REPEAT] = process.argv.slice(2, 5).map(it => parseInt(it))
+const [INPUT_RATE, DURATION_TIME, REPEAT, SLICE] = process.argv.slice(2, 6).map(it => parseInt(it))    //test version
 
 const block_commit_time = fs.readFileSync(config.tendermint.path.block_commit_time, 'utf-8').split('\n').slice(0, -1)  // 28,50,1568105264997 => block_number,transaction_number,block_commit_time
 const transaction_log = require(config.tendermint.path.transaction_log)
-const txs = Object.keys(require(config.tendermint.path.raw_tx_hash))
+const txs = Object.values(require(config.tendermint.path.raw_tx_hash)).slice(SLICE)   //SLICE for test
 
 const app = {
   block_info: {},
@@ -15,7 +16,7 @@ const app = {
 }
 
 for (let block of block_commit_time) {
-  let [height, num_txs, timestamp] = it.split(',')
+  let [height, num_txs, timestamp] = block.split(',')
 
   app.block_info[`block_${height}`] = {
     num_txs: num_txs,
@@ -29,8 +30,8 @@ for (let block of block_commit_time) {
   while (app.repeat_count < REPEAT) {
     let rlt = {}
 
-    for (let i = app.txs_in_one_period * app.repeat_count; i < app.txs_in_one_period * (app.repeat_count + 1); i++) {
-      let tx_block_height = JSON.parse(await getTransactionReceipt(config.tendermint.urls[0], txs[i])).result.height
+    for (let i = app.txs_in_one_period * app.repeat_count; i < app.txs_in_one_period * (app.repeat_count + 1); i++) { 
+      let tx_block_height = JSON.parse(await getTransactionReceipt(config.tendermint.urls[0], txs[i])).result.height  
 
       if (undefined === rlt[`block_${tx_block_height}`]) {
         rlt[`block_${tx_block_height}`] = JSON.parse(JSON.stringify(app.block_info[`block_${tx_block_height}`]))
